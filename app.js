5ce7ace0001381295ff7c693d25d911aaff14e6e
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-10-04 11:26:50 -0500

    created: if the user press buttons more than 7 times then it display you dead

--- app.js
+++ app.js
@@ -1,51 +1,3 @@
-
-
-
-// console.log(displayWord(random));
-/////////////////////////////////////////////
-
-
-// const creatAlphabetBtns = () => {
-//     let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L',
-//                 'M','N','O','P','Q','R','S','T','U','V','W','X',
-//                 'Y','Z'];
-
-//     $('body').append('<div class = letterDiv>');
-
-//     for(let i = 0; i < alphabet.length; i++){
-//         // let $btn = $('body').append('<button/>');
-//         // let $x = alphabet[i];
-//         $('.letterDiv').append('<button class = letterBtns>');
-
-
-//         // let alphabetButton = document.createElement('button');
-//         // alphabetButton.innerHTML = alphabet[i];
-//         // alphabetButton.className = 'letterBtn';
-//         // alphabetButton.id = 'check';
-//         // // console.log(alphabetButton);
-
-//         // $('#letterButtons').append(alphabetButton)
-//     }
-
-
-
-//     $('.letterBtn').on('click', () =>{
-//         let x = $('#check').val();
-//         console.log(x);
-//     })
-// }
-
-// creatAlphabetBtns();
-
-// const dog = () =>{
-//         $('.alphabetButton').on('click', () => {
-//             let geuss = (this.innerHTML);
-//             console.log(geuss);
-//         });
-// }
-// dog();
-
-
 let holdWord = [];
 let answerArray = [];
 let holdLetter = [];
@@ -60,45 +12,52 @@ const wordArray = () =>{
 
 
 
-    const random = words[Math.floor((Math.random() * words.length ))];
+    const random = words[Math.floor(Math.random() * words.length )];
     //// pushing the random word to holdWord and then split it
     holdWord = random.split('');
     // compareInput(holdWord);
     // console.log(holdWord);
         //console.log(random);
     $('.button').on('click', () =>{
-        displayWord(random);
+        // displayWord(random);
        
         // $('p').text(newArray);
         //console.log(newArray);
-        console.log(holdWord);
+        // console.log(holdWord);
         for(let i = 0; i < holdWord.length; i++){
              // $('.guessWord').append('<p class="'+ holdWord[i] + ' "> '+"_"+' </p>');
-             answerArray[i] = '_';
+             answerArray[i] = ('_');
+             // OR
+             // answerArray.push('_');
              //// ??????????????????
              $('.guessWord').append('<p class = "'+ holdWord[i] + ' "> '+ answerArray[i] + '</p>');
         }
     });
     ////////DISPLAY THE RANDOM WORD
-    const displayWord = (random) =>{
-        $('p').text(random);
-    }
+    // const displayWord = (random) =>{
+    //     $('p').text(random);
+    // }
 }
 wordArray();
 
-
 const keyboard = () =>{
+
     $('.letterBtn').on('click', function(){
+
         ////// ????????????????
         var buttonPressed = $(this).html();
         // console.log(buttonPressed);
         compareInput(buttonPressed);
+
     });
 }
 keyboard();
 
 
-const compareInput = (buttonPressed) =>{
+var tries = 7;
+var winner = []
+
+const compareInput = (buttonPressed, event) =>{
     // console.log(newArray);
     // console.log(buttonPressed);
     // console.log(holdWord);
@@ -106,22 +65,109 @@ const compareInput = (buttonPressed) =>{
            // console.log(buttonPressed);
            //  console.log(holdWord[i]);
            //  console.log(answerArray[i]);
-           //  console.log(answerArray);
-        if(buttonPressed === holdWord[i]){
+           //  console.log(answerArray); 
+
+
+       if(buttonPressed === holdWord[i]){
             // answerArray[i] = holdWord[i];
+
             ////// ???????????????
-            $('.' + buttonPressed).text(buttonPressed);
-
-
+           var holdPressBtn = $('.' + buttonPressed).text(buttonPressed);
+           console.log(holdPressBtn);
+            // alert('right answer');
+            winner[i]++;
+            
 
             // console.log('letter is in word');
-        }else {
-            console.log('try again');
         }
+  
     }
+
+
+
+    //// if answerArray does not include the buttonPress
+    //// then decremeant the tries 
+    if(!answerArray.includes(holdPressBtn)) {
+        tries--;
+    }
+    ///// if the tries reached zero then print You dead
+    if(tries === 0){
+        console.log('ran out of tries');
+        $('body').append('<div class = noMoreTries></div>');
+        $('.noMoreTries').text('You Dead!!!!!!');
+    }
+
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+// $(document).ready(function(){
+
+//     ////  words array holds all the words
+//     const words = ['cat','dog','tiger','drink','cannon','eight','plane'];
+//     ////get a random word from the array and assign it to random
+//     const random = words[Math.floor((Math.random() * words.length ))];
+//     //// split the random word and assign it to holdWord
+//     holdWord = random.split('');
+//     console.log(holdWord);
+
+//     //// Keyboard buttons click function
+//     var wrongGuesses = 0;
+//     $('.letterBtn').on('click', () =>{
+//         $(this).addClass('used');
+//         ///// ??????????????????
+//         $(this).prop('disabled','true');
+//          var matchFound = false;
+
+
+//          //// check if clicked letter is in secret word
+//          var userGuess = $(this).text();
+//          for(var i = 0; i < random.length; i++){
+//             if(userGuess === random[i]){
+
+//             $('#container').find(":nth-child(" + (i + 1) )
+//             ////// ???????????????
+//             $('.' + buttonPressed).text(buttonPressed);
+
+//         }
+//          }
+
+//     });
+
+
+// });
 
 
 
